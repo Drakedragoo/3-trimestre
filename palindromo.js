f21b5ea945252013e304df3a054a1475a148c52f
Drakedragoo <163123866+Drakedragoo@users.noreply.github.com>
2024-09-13 11:09:58 -0300

    Add files via upload

--- palindromo.js
+++ palindromo.js
@@ -1,59 +1,12 @@
-function calcular(num1, num2, operacao,) {
-    switch (operacao) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        case '/':
-            if (num2 !== 0) {
-                return num1 / num2;
-            } else {
-                throw new Error("Divisão por zero não é permitida.");
-            }
-        default:
-            throw new Error("Operação inválida: " + operacao);
-    }
+function ehPalindromo(str) {
+    str = str.replace(/\s+/g,'').toLowerCase();
+
+    return str === str.split('').reverse().join('');
 }
 
-function ePalindromo(texto) {
-    const textoLimpo = texto.replace(/\s+/g, '').toLowerCase();
-    const comprimento = textoLimpo.length;
-
-    for (let i = 0; i < comprimento / 2; i++) {
-        if (textoLimpo[i] !== textoLimpo[comprimento - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
-function calcularEVerificarPalindromo(num1, num2, operacao) {
-    try {
-        const resultado = calcular(num1, num2, operacao);
-        const resultadoComoTexto = resultado.toString();
-        const palindromo = ePalindromo(resultadoComoTexto);
-        return {
-            resultado: resultado,
-            ePalindromo: palindromo
-        };
-    } catch (error) {
-        return {
-            erro: error.message
-        };
-    }
-}
-
-const num1 = 121;
-const num2 = 121;
-const operacao = '+';
-
-const resultado = calcularEVerificarPalindromo(num1, num2, operacao);
-if (resultado.erro) {
-    console.error("Erro:", resultado.erro);
+const frase = "A man a plan a canal Panama";
+if (ehPalindromo(frase)) {
+    console.log(`${frase} é um palíndromo.`);
 } else {
-    console.log("Resultado:", resultado.resultado);
-    console.log("É palíndromo:", resultado.ePalindromo);
+    console.log(`${frase} não é um palíndromo.`);
 }
